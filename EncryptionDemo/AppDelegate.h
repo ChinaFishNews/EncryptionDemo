8d0924d490f16ed55d095fa1474b37bf7dbf5a58
yuxinwen <yuxinwen@lvmama.com>
2018-06-24 22:44:10 +0800

    Initial Commit

---
+++ EncryptionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EncryptionDemo
+//
+//  Created by 新闻 on 2018/6/24.
+//  Copyright © 2018年 新闻. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EncryptionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EncryptionDemo
+//
+//  Created by 新闻 on 2018/6/24.
+//  Copyright © 2018年 新闻. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
