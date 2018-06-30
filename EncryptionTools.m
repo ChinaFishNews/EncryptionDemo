dfc8bb06c756b6b7ae681102d78524d36c572325
yuxinwen <yuxinwen@lvmama.com>
2018-06-30 16:58:47 +0800

    encryption

---
+++ EncryptionTools.m
@@ -0,0 +1,153 @@
+//
+//  EncryptionTools.m
+//
+
+#import "EncryptionTools.h"
+
+@interface EncryptionTools()
+@property (nonatomic, assign) int keySize;
+@property (nonatomic, assign) int blockSize;
+@end
+
+@implementation EncryptionTools
+
++ (instancetype)sharedEncryptionTools {
+    static EncryptionTools *instance;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[self alloc] init];
+        instance.algorithm = kCCAlgorithmAES;
+    });
+    
+    return instance;
+}
+
+- (void)setAlgorithm:(uint32_t)algorithm {
+    _algorithm = algorithm;
+    switch (algorithm) {
+        case kCCAlgorithmAES:
+            self.keySize = kCCKeySizeAES128;
+            self.blockSize = kCCBlockSizeAES128;
+            break;
+        case kCCAlgorithmDES:
+            self.keySize = kCCKeySizeDES;
+            self.blockSize = kCCBlockSizeDES;
+            break;
+        default:
+            break;
+    }
+}
+
+- (NSString *)encryptString:(NSString *)string keyString:(NSString *)keyString iv:(NSData *)iv {
+    
+    // 设置秘钥
+    NSData *keyData = [keyString dataUsingEncoding:NSUTF8StringEncoding];
+    uint8_t cKey[self.keySize];
+    bzero(cKey, sizeof(cKey));
+    [keyData getBytes:cKey length:self.keySize];
+    
+    // 设置iv
+    uint8_t cIv[self.blockSize];
+    bzero(cIv, self.blockSize);
+    int option = 0;
+    if (iv) {
+        [iv getBytes:cIv length:self.blockSize];
+        option = kCCOptionPKCS7Padding;
+    } else {
+        option = kCCOptionPKCS7Padding | kCCOptionECBMode;
+    }
+    
+    // 设置输出缓冲区
+    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+    size_t bufferSize = [data length] + self.blockSize;
+    void *buffer = malloc(bufferSize);
+    
+    // 开始加密
+    size_t encryptedSize = 0;
+    /*
+    *  @param kCCEncrypt     加密/解密
+    *  @param self.algorithm 加密算法
+    *  @param option         CBC/ECB
+    *  @param cKey           加密密钥
+    *  @param self.keySize   密钥长度
+    *  @param cIv            iv初始化向量
+    *  @param bytes]         加密的数据
+    *  @param length]        加密的数据长度
+    *  @param buffer         密文的缓冲区
+    *  @param bufferSize     缓冲区的大小
+    *  @param encryptedSize  加密结果的大小
+    */
+    CCCryptorStatus cryptStatus = CCCrypt(kCCEncrypt,
+                                          self.algorithm,
+                                          option,
+                                          cKey,
+                                          self.keySize,
+                                          cIv,
+                                          [data bytes],
+                                          [data length],
+                                          buffer,
+                                          bufferSize,
+                                          &encryptedSize);
+    
+    NSData *result = nil;
+    if (cryptStatus == kCCSuccess) {
+        result = [NSData dataWithBytesNoCopy:buffer length:encryptedSize];
+    } else {    
+        free(buffer);
+        NSLog(@"[错误] 加密失败|状态编码: %d", cryptStatus);
+    }
+    
+    return [result base64EncodedStringWithOptions:0];
+}
+
+- (NSString *)decryptString:(NSString *)string keyString:(NSString *)keyString iv:(NSData *)iv {
+    
+    // 设置秘钥
+    NSData *keyData = [keyString dataUsingEncoding:NSUTF8StringEncoding];
+    uint8_t cKey[self.keySize];
+    bzero(cKey, sizeof(cKey));
+    [keyData getBytes:cKey length:self.keySize];
+    
+    // 设置iv
+    uint8_t cIv[self.blockSize];
+    bzero(cIv, self.blockSize);
+    int option = 0;
+    if (iv) {
+        [iv getBytes:cIv length:self.blockSize];
+        option = kCCOptionPKCS7Padding;
+    } else {
+        option = kCCOptionPKCS7Padding | kCCOptionECBMode;
+    }
+    
+    // 设置输出缓冲区
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:string options:0];
+    size_t bufferSize = [data length] + self.blockSize;
+    void *buffer = malloc(bufferSize);
+    
+    // 开始解密
+    size_t decryptedSize = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt,
+                                          self.algorithm,
+                                          option,
+                                          cKey,
+                                          self.keySize,
+                                          cIv,
+                                          [data bytes],
+                                          [data length],
+                                          buffer,
+                                          bufferSize,
+                                          &decryptedSize);
+    
+    NSData *result = nil;
+    if (cryptStatus == kCCSuccess) {
+        result = [NSData dataWithBytesNoCopy:buffer length:decryptedSize];
+    } else {
+        free(buffer);
+        NSLog(@"[错误] 解密失败|状态编码: %d", cryptStatus);
+    }
+    
+    return [[NSString alloc] initWithData:result encoding:NSUTF8StringEncoding];
+}
+
+@end
