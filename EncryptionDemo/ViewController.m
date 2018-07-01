337a3e0d4bda2b64439eaaabea89e6c3dd551199
yuxinwen <yuxinwen@lvmama.com>
2018-07-01 22:56:10 +0800

    rsa

--- EncryptionDemo/ViewController.m
+++ EncryptionDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "RSACryptor.h"
 
 @interface ViewController ()
 
@@ -16,13 +17,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // 加载公钥
+    [[RSACryptor sharedRSACryptor] loadPublicKey:[[NSBundle mainBundle] pathForResource:@"rsacert.der" ofType:nil]];
+    // 加载私钥 - P12的文件  password : 生成P12 的时候设置的密码
+    [[RSACryptor sharedRSACryptor] loadPrivateKey:[[NSBundle mainBundle] pathForResource:@"p.p12" ofType:nil] password:@"123456"];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSData * reault = [[RSACryptor sharedRSACryptor] encryptData:[@"hello" dataUsingEncoding:NSUTF8StringEncoding]];
+    NSString * base64 = [reault base64EncodedStringWithOptions:0];
+    NSLog(@"加密的信息: %@",base64);
+    
+    // 解密
+    NSData * jiemi = [[RSACryptor sharedRSACryptor] decryptData:reault];
+    NSLog(@"%@",[[NSString alloc]initWithData:jiemi encoding:NSUTF8StringEncoding]);
+    
 }
 
 
